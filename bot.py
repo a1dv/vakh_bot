f648a2aa1f9fdf839035a66f3bd4dff0f822eefc
a1dv <52347696+a1dv@users.noreply.github.com>
2021-06-21 00:08:17 +0300

    Done

---
+++ bot.py
@@ -0,0 +1,19 @@
+# - *- coding: utf- 8 - *-
+import telebot
+import sys
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+TOKEN = 'YOUR_TOKEN'
+driver.get('http://www.gatchina.biz/generator')
+
+search_input = driver.find_elements_by_css_selector('input')[0]
+search_input.send_keys('Вахрамян')
+login_form = driver.find_element_by_xpath("/html/body/form/table/tbody/tr/td/a")
+login_form.click();
+sub_menus_html = driver.find_element_by_css_selector('div#text')
+res = sub_menus_html.text
+result = res[:res.find("А вот «Нямархав»")]
+driver.close()
+bot.send_message('@vakhramyanko', result)
+sys.exit()
